478374f01cf6b765d974a1ad5c4e92c1ff5ac0dc
Plx15 <pierre@rible.com>
2022-05-22 19:26:53 +0200

    convert to utf-8

--- projetV2/progallegro.c
+++ projetV2/progallegro.c
@@ -3,12 +3,12 @@
 
 BITMAP* recup_sprites( BITMAP*scr,int w,int h,int startx,int starty,int col,int element)
 // bitmap origine
-// taille de l’image élément d’une animation
+// taille de lï¿½image ï¿½lï¿½ment dï¿½une animation
 //
-// début de la séquence voulue à partir de la position
+// dï¿½but de la sï¿½quence voulue ï¿½ partir de la position
 // x et y
 // nombre de colonnes dans la bitmap origine
-// numéro de l’image voulue dans la séquence
+// numï¿½ro de lï¿½image voulue dans la sï¿½quence
 
 {
 
@@ -17,16 +17,16 @@ BITMAP* recup_sprites( BITMAP*scr,int w,int h,int startx,int starty,int col,int 
     int x,y;
 
     bmp=create_bitmap(w,h);
-    // création d’une bitmap pour le sprite
+    // crï¿½ation dï¿½une bitmap pour le sprite
 
     if (bmp!=NULL)
     {
 
         x = startx + (element%col)*w;
-        // calcul de la position réelle du sprite dans la bitmap
+        // calcul de la position rï¿½elle du sprite dans la bitmap
 
         y = starty + (element/col)*h;
-        // d’origine
+        // dï¿½origine
 
         blit( scr, bmp, x, y, 0, 0, w, h);
         //recopie du sprite voulu
